b1f4fcbd31094961c81a8e174abaeeb85222ddeb
jacaraba <jacaraba@hotmail.com>
2023-03-14 11:05:32 -0500

    inicio

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'u392441939_amigos';
+	$dbPassword = 'Timo2195';
+	$dbDatabase = 'u392441939_amigos';
+	$dbPort = '3306';
+	$appURI = 'amigos';
+	$host = 'amigos2023.online';
+
+	$adminConfig = [
+		'adminUsername' => "jacaraba",
+		'adminPassword' => "\$2y\$10\$dIKGcMAdN12OL32mdQa4du2YEoUKGiq0zDlSZcxHMxcWzURDa2Sge",
+		'senderEmail' => "jacaraba@hotmail.com",
+		'notifyAdminNewMembers' => "",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Full Name",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "State",
+		'MySQLDateFormat' => "%m/%d/%Y",
+		'PHPDateFormat' => "n/j/Y",
+		'PHPDateTimeFormat' => "m/d/Y, h:i a",
+		'senderName' => "Membership management",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\n\nYour membership is now approved by the admin. You can log in to your account here:\nhttp://amigos2023.online/amigos\n\nRegards,\nAdmin",
+		'hide_twitter_feed' => "",
+		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
+		'mail_function' => "mail",
+		'smtp_server' => "",
+		'smtp_encryption' => "",
+		'smtp_port' => "25",
+		'smtp_user' => "",
+		'smtp_pass' => "",
+		'googleAPIKey' => "",
+		'baseUploadPath' => "images",
+	];
