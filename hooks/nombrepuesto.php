df2481c051ac1677da8979db814f95c30c403a49
jacaraba <jacaraba@hotmail.com>
2023-03-14 12:06:22 -0500

    inicio

---
+++ hooks/nombrepuesto.php
@@ -0,0 +1,32 @@
+<?php
+// Recibe el parámetro POST "direccion"
+$direccion = $_POST["direccion"];
+
+// Conexión a la base de datos MySQL
+$host = "localhost";
+$user = "u392441939_amigos";
+$password = "Timo2195";
+$dbname = "u392441939_amigos";
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+// Verifica la conexión a la base de datos
+if (!$conn) {
+  die("Conexión fallida: " . mysqli_connect_error());
+}
+
+// Consulta a la base de datos
+$sql = "SELECT * FROM `divpol2022` LIMIT 1";
+$resultado = mysqli_query($conn, $sql);
+
+// Crea un arreglo con los resultados de la consulta
+$amigos = array();
+while ($fila = mysqli_fetch_assoc($resultado)) {
+  $amigos[] = $fila;
+}
+
+// Convierte el arreglo en formato JSON y lo muestra en pantalla
+echo json_encode($amigos);
+
+// Cierra la conexión a la base de datos
+mysqli_close($conn);
+?>
