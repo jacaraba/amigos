a781a3b5730475059f638cfd90f19d54af76d427
jacaraba <jacaraba@hotmail.com>
2023-03-14 11:10:06 -0500

    inicio

---
+++ hooks/header-extras.php
@@ -0,0 +1,3 @@
+<div style="background-color: #f2f2f2; border: 2px solid #ccc; border-radius: 10px; display: inline-block; padding: 10px;">
+			  <a style="color: #333; font-size: 18px; text-decoration: none;" href="https://wsp.registraduria.gov.co/censo/consultar?record-added-ok=1733087395" target="_blank">Consultar PUESTO DE VOTACION</a>
+</div>
